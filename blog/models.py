85bd4db3d5487b776a4d17e8ca88402aa8bb366f
faridhaavasi <faridhavasi777@gmail.com>
2023-02-25 01:19:09 +0330

    first model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
