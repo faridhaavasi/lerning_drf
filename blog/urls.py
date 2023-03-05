2a1bee11699f1c6ddcde01e467adf1367ea83507
faridhaavasi <faridhavasi777@gmail.com>
2023-03-05 17:33:35 +0330

    reset and review

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-   path('list', views.PostlistviewApi.as_view()),
-   path('detail/<int:pk>', views.detailpostviewApi.as_view()),
-   path('add', views.addpostApi.as_view())
-]
